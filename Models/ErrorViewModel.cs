1a1da7ad4b19dc23b6427fd68180396e4ae6961c
Kevin Johr <kevin.johr@posteo.de>
2024-03-22 13:56:40 +0100

    Projektdateien hinzufügen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_Quizz_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
