a30b002ede62fa4b1ad01fd7da19a1dc85951a38
Pylz <pilleg@yahoo.de>
2024-03-26 20:54:24 +0100

    Neue Funktionalitäten in HomeController.cs hinzugefügt und zwei neue Model-Klassen Answer und QuizQuestion erstellt. HomeController.cs enthält jetzt zwei neue Aktionsmethoden CreateQuiz für GET- und POST-Anfragen. Die GET-Methode zeigt das Quiz-Erstellungsformular an, während die POST-Methode die Formularübermittlung behandelt. Die Model-Klassen Answer und QuizQuestion wurden hinzugefügt, um eine Antwort auf eine Quizfrage bzw. eine Quizfrage darzustellen.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,37 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
 using Project_Quizz_Frontend.Models;
 using System.Diagnostics;
-using Project_Quizz_Frontend.Areas.Identity.Pages.Account;
 
 namespace Project_Quizz_Frontend.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            if (User.Identity?.IsAuthenticated != true)
-            {
-                return RedirectToPage("/Account/Login", new { area = "Identity" });
-            }
-            return View();
-        }
+		public IActionResult Index()
+		{
+			if (User.Identity?.IsAuthenticated != true)
+			{
+				return RedirectToPage("/Account/Login", new { area = "Identity" });
+			}
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		[HttpGet]
+		public IActionResult CreateQuiz()
+		{
+			var model = new QuizQuestion();
+			return View("~/Views/Quiz/CreateQuiz.cshtml", model);
+		}
+
+		[HttpPost]
+		public IActionResult CreateQuiz(QuizQuestion quizQuestion, int correctAnswer)
+		{
+			if (ModelState.IsValid)
+			{
+				// Logic to handle the quizQuestion and correctAnswer
+				return RedirectToAction("Index");
+			}
+
+			return View("~/Views/Quiz/CreateQuiz.cshtml", quizQuestion);
+		}
+	}
 }
