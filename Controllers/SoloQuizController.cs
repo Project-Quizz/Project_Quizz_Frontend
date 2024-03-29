939453ced85496223ae7f4244ff215862d63b49a
Pylz <pilleg@yahoo.de>
2024-03-29 21:26:28 +0100

    Die wichtigsten Änderungen betreffen die Einführung eines neuen Features für ein Einzelspieler-Quiz. Dazu gehören ein neuer Button auf der Index.cshtml-Seite, ein neuer Controller (SoloQuizController.cs), ein neues Modell (SoloQuizModel.cs) und eine neue Ansicht (SoloQuiz.cshtml).
    1. Ein neuer Button wurde zur Index.cshtml-Seite hinzugefügt, um Benutzern den Zugriff auf das Einzelspieler-Quiz zu ermöglichen.
    
    2. Der neue Controller, SoloQuizController.cs, verwaltet die Quiz-Funktionalität, einschließlich Anzeige, Antwortübermittlung und Navigation zwischen den Fragen.
    
    3. Das neue Modell, SoloQuizModel.cs, repräsentiert die Quiz-Struktur mit Fragen, Antworten und dem aktuellen Quiz-Status.
    
    4. Die neue Ansicht, SoloQuiz.cshtml, zeigt das Quiz an und ermöglicht Benutzern das Beantworten von Fragen, Navigieren durch das Quiz und zeigt Feedback zu den Antworten an.
    
    Diese Änderungen bringen das neue Einzelspieler-Quiz-Feature.

---
+++ Controllers/SoloQuizController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Project_Quizz_Frontend.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project_Quizz_Frontend.Controllers
+{
+	public static class SessionExtensions
+	{
+		public static void Set<T>(this ISession session, string key, T value)
+		{
+			session.SetString(key, JsonConvert.SerializeObject(value));
+		}
+
+		public static T Get<T>(this ISession session, string key)
+		{
+			var value = session.GetString(key);
+			return value == null ? default : JsonConvert.DeserializeObject<T>(value);
+		}
+	}
+
+	public class SoloQuizController : Controller
+	{
+		public IActionResult SoloQuiz()
+		{
+			var quiz = new SoloQuizModel
+			{
+				QuestionId = 1,
+				QuestionText = "What is the capital of France?",
+				Answers = new List<QuizAnswerModel>
+				{
+					new QuizAnswerModel { AnswerId = 1, AnswerText = "Paris", IsCorrect = true },
+					new QuizAnswerModel { AnswerId = 2, AnswerText = "Berlin", IsCorrect = false },
+					new QuizAnswerModel { AnswerId = 3, AnswerText = "Madrid", IsCorrect = false },
+					new QuizAnswerModel { AnswerId = 4, AnswerText = "Rome", IsCorrect = false }
+				}
+			};
+
+			HttpContext.Session.Set("quiz", quiz);
+			return View("~/Views/Quiz/SoloQuiz.cshtml", quiz);
+		}
+
+		[HttpPost]
+		public IActionResult SubmitAnswer(int selectedAnswerId, string action)
+		{
+			var quiz = HttpContext.Session.Get<SoloQuizModel>("quiz");
+			var selectedAnswer = quiz.Answers.FirstOrDefault(a => a.AnswerId == selectedAnswerId);
+			bool isCorrect = selectedAnswer != null && selectedAnswer.IsCorrect;
+
+			// Set the IsAnswerCorrect property
+			quiz.IsAnswerCorrect = isCorrect;
+
+			switch (action)
+			{
+				case "restart":
+					return RedirectToAction("SoloQuiz");
+				case "next":
+					if (quiz.HasNextQuiz)
+					{
+						return RedirectToAction("NextQuiz");
+					}
+					break;
+			}
+			return View("~/Views/Quiz/SoloQuiz.cshtml", quiz);
+		}
+
+
+		public IActionResult NextQuiz()
+		{
+			var nextQuiz = new SoloQuizModel
+			{
+				QuestionId = 2,
+				QuestionText = "What is the capital of Germany?",
+				Answers = new List<QuizAnswerModel>
+				{
+					new QuizAnswerModel { AnswerId = 5, AnswerText = "Paris", IsCorrect = false },
+					new QuizAnswerModel { AnswerId = 6, AnswerText = "Rome", IsCorrect = false },
+					new QuizAnswerModel { AnswerId = 7, AnswerText = "Berlin", IsCorrect = true },
+					new QuizAnswerModel { AnswerId = 8, AnswerText = "Tokyo", IsCorrect = false }
+				},
+			};
+			HttpContext.Session.Set("quiz", nextQuiz);
+			return View("~/Views/Quiz/SoloQuiz.cshtml", nextQuiz);
+		}
+	}
+}
---
+++ Models/SoloQuizModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_Quizz_Frontend.Models // Replace with your actual namespace
+{
+	public class SoloQuizModel
+	{
+		public int Id { get; set; }
+
+		[Required] public string QuestionText { get; set; }
+
+		public List<QuizAnswerModel> Answers { get; set; } = new List<QuizAnswerModel>();
+
+		// Indicate if there is a next quiz available (it is available if the current QuestionId is less than the total number of questions)
+		public int QuestionId { get; set; }
+
+		// The ID of the selected answer
+		public int SelectedAnswerId { get; set; }
+
+		// Total number of questions, hardcoded to 2 for now
+		public int TotalQuestions { get; set; } = 2;
+
+		// Update HasNextQuiz to check if QuestionId is less than TotalQuestions
+		public bool HasNextQuiz => QuestionId < TotalQuestions;
+		public bool? IsAnswerCorrect { get; set; }
+	}
+
+	public class QuizAnswerModel
+	{
+		public int AnswerId { get; set; }
+
+		public string AnswerText { get; set; }
+
+		public bool IsCorrect { get; set; }
+	}
+}
