e5eefc16b6fa789c16b04e0d726749a55484ddbc
Kevin Johr <kevin.johr@posteo.de>
2024-04-10 16:25:57 +0200

    feedback implementation

---
+++ Controllers/QuestionFeedbackController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Project_Quizz_Frontend.Models;
+using Project_Quizz_Frontend.Services;
+
+namespace Project_Quizz_Frontend.Controllers
+{
+	public class QuestionFeedbackController : Controller
+	{
+		private readonly QuizApiService _quizApiService;
+		private readonly UserManager<IdentityUser> _userManager;
+
+        public QuestionFeedbackController(QuizApiService quizApiService, UserManager<IdentityUser> userManager)
+        {
+            _quizApiService = quizApiService;
+			_userManager = userManager;
+        }
+
+		public async Task<IActionResult> CreateFeedbackForQuestion(int questionId, string feedback)
+		{
+			var feedbackDto = new CreateQuizQuestionFeedbackDto
+			{
+				QuestionId = questionId,
+				Feedback = feedback,
+				UserId = _userManager.GetUserId(User)
+			};
+
+			var response = await _quizApiService.CreateFeedbackForQuestion(feedbackDto);
+
+			if (response.IsSuccessStatusCode)
+			{
+				return Json(new { success = true });
+			}
+
+			return Json(new { success = false });
+		}
+	}
+}
---
+++ Models/FeedbackForQuestionDtos.cs
@@ -0,0 +1,17 @@
+﻿namespace Project_Quizz_Frontend.Models
+{
+	public class CreateQuizQuestionFeedbackDto
+	{
+		public int QuestionId { get; set; }
+		public string Feedback { get; set; }
+		public string UserId { get; set; }
+	}
+
+	public class GetQuizQuestionFeedbackDto
+	{
+		public int QuestionId { get; set; }
+		public string Feedback { get; set; }
+		public string UserId { get; set; }
+		public DateTime CreateDate { get; set; }
+	}
+}
