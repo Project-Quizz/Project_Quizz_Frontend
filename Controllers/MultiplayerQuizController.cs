d7fdf2ff8b276796f897e44a5a6d03cc86a977a1
Kevin Johr <kevin.johr@posteo.de>
2024-04-11 23:45:01 +0200

    final highscore implementation and bug fixes

--- Controllers/MultiplayerQuizController.cs
+++ Controllers/MultiplayerQuizController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Project_Quizz_Frontend.Models;
 using Project_Quizz_Frontend.Services;
@@ -9,6 +11,7 @@ using System.Net;
 
 namespace Project_Quizz_Frontend.Controllers
 {
+    [Authorize]
     public class MultiplayerQuizController : Controller
     {
 		private readonly MultiplayerApiService _multiQuizApiService;
@@ -175,6 +178,12 @@ namespace Project_Quizz_Frontend.Controllers
             var quizQuestionJson = HttpContext.Session.GetString("MultiQuizQuestion");
             var quizQuestion = JsonConvert.DeserializeObject<GetQuizQuestionDto>(quizQuestionJson);
             var answers = quizQuestion.Answers.Where(x => selectedAnswerIds.Contains(x.Id)).ToList();
+
+            if (selectedAnswerIds.IsNullOrEmpty())
+            {
+                TempData["ErrorMessageBadRequest"] = "Bitte wähle mindestens eine Antwort aus!";
+                return View("MultiQuizSession", quizQuestion);
+            }
 
             var updateMultiQuizSessionObj = new UpdateMultiQuizSessionDto
             {
